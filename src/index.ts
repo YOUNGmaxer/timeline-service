1f02785747926cb24525cc6a1c11e7bf07a277e3
YOUNGmaxer <517538470@qq.com>
2024-07-07 09:08:04 +0000

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(60000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .listen(60000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
